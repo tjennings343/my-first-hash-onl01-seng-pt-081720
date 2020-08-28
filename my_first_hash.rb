e8e31b028b1cc3e346add6b2ef9ee5c41ab2736f
Trey Jennings <tjennings343@gmail.com>
2020-08-28 02:35:16 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -3,8 +3,11 @@ my_var = {"name" => "Trey", "age" => "28"}
 end
 
 
-def shipping_manifest(the_manifest)
-  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+def shipping_manifest
+  the_manifest = {
+    "whale bone corsets" => 5, 
+    "porcelain vases" => 2, 
+    "oil paintings" => 3}
   "We have #{the_manifest}"
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information:
